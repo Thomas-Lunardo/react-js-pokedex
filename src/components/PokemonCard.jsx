4fc4e4b2a8e38726c8d19492075572e561fec9b2
THOMAS <thomaslunardo@gmail.com>
2024-02-18 17:46:35 +0100

    need to pull

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return <figure>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokémon Bulbasaur"/>
         <figcaption>Bulbasaur</figcaption>
     </figure>;
   }
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   
   export default PokemonCard;
