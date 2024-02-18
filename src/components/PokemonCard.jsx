c4f7c4060acbce9ade67e361eadd66279df6975f
THOMAS <thomaslunardo@gmail.com>
2024-02-18 17:28:27 +0100

    add style

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,10 @@
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokémon Bulbasaur"/>
+    return <figure className="card">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="bulbasaur"
+        className="card-img"
+      />
         <figcaption>Bulbasaur</figcaption>
     </figure>;
   }
