09f1037a3fece45215faf10aca3860c7e6b5c32d
THOMAS <thomaslunardo@gmail.com>
2024-02-18 22:47:03 +0100

    finish the props challenge

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,9 @@
-function PokemonCard() {
-    const pokemon = pokemonList[2];
+function PokemonCard({pokemon}) {
 
     return <figure className="card">
         {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img"/> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
     </figure>;
   }
-
-  const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "Mew",
-    },
-    {
-      name: "Pykoduck",
-      imgSrc:
-        "https://w7.pngwing.com/pngs/227/903/png-transparent-pokemon-pysduck-pokemon-go-pikachu-misty-psyduck-pokemon-orange-pokemon-pokemon-png-thumbnail.png",
-    },
-  ];
   
   export default PokemonCard;
